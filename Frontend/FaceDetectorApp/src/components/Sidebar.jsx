0f68f6656dad1a3cf88f2062e96401702afeec8c
Robin Lassak <lassak.robin@gmail.com>
2025-05-17 14:08:39 +0200

    Uprava sidebaru - accordions

--- Frontend/FaceDetectorApp/src/components/Sidebar.jsx
+++ Frontend/FaceDetectorApp/src/components/Sidebar.jsx
@@ -5,33 +5,88 @@ function Sidebar() {
   return (
     <div className="bg-dark text-white vh-100 p-3" style={{ width: "220px" }}>
       <h4 className="mb-4 text-center">Face Detector</h4>
-      <ul className="nav nav-pills flex-column">
-        <li className="nav-item mb-2">
-          <Link className="nav-link text-white" to="/Search">
-            Search
-          </Link>
-        </li>
-        <li className="nav-item mb-2">
-          <Link className="nav-link text-white" to="/Profiles">
-            Profiles
-          </Link>
-        </li>
-        <li className="nav-item mb-2">
-          <Link className="nav-link text-white" to="/Edit">
-            Edit
-          </Link>
-        </li>
-        <li className="nav-item mb-2">
-          <Link className="nav-link text-white" to="/Update">
-            Update
-          </Link>
-        </li>
-        <li className="nav-item mb-2">
-          <Link className="nav-link text-white" to="/People-Database">
-            People Database
-          </Link>
-        </li>
-      </ul>
+
+      {/*Prvni Accordion*/}
+      <div className="accordion" id="sidebarAccordion">
+        <div className="accordion-item bg-dark border-0">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed bg-dark text-white"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#searchMenu"
+            >
+              Search
+            </button>
+          </h2>
+          <div
+            id="searchMenu"
+            className="accordion-collapse collapse"
+            data-bs-parent="#sidebarAccordion"
+          >
+            <div className="accordion-body p-0">
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2">
+                  <Link className="nav-link text-white" to="/SearchByPhoto">
+                    Photo
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link className="nav-link text-white" to="/SearchByVideo">
+                    Video
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/*Druhy Accordion*/}
+      <div className="accordion" id="sidebarAccordion2">
+        <div className="accordion-item bg-dark border-0">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed bg-dark text-white"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#managementMenu"
+            >
+              Management
+            </button>
+          </h2>
+          <div
+            id="managementMenu"
+            className="accordion-collapse collapse"
+            data-bs-parent="#sidebarAccordion2"
+          >
+            <div className="accordion-body p-0">
+              <ul className="nav nav-pills flex-column">
+                <li className="nav-item mb-2">
+                  <Link className="nav-link text-white" to="/Profiles">
+                    Profiles
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link className="nav-link text-white" to="/Edit">
+                    Edit
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link className="nav-link text-white" to="/Update">
+                    Update
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link className="nav-link text-white" to="/People-Database">
+                    People Database
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
